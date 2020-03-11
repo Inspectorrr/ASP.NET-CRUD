d6e7a971335a4618bd343f5fb5f3a5ddbf86b14e
Ilya Vins <iluarez12@gmail.com>
2020-03-11 10:17:36 +0200

    First commit

---
+++ NetCrud/Contact.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Management;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace NetCrud
+{
+    public partial class Contact : System.Web.UI.Page
+    {
+
+        SqlConnection sqlCon = new SqlConnection(@"Data Source=RAX-2\SQLEXPRESS;Initial Catalog=CRUD;Integrated Security=True;");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                btnDelete.Enabled = false;
+                FillGridView();
+            }
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        public void Clear()
+        {
+            hfContactID.Value = "";
+            txtName.Text = txtMobile.Text = txtAddress.Text = "";
+            lblSuccessMessage.Text = lblErrorMessage.Text = "";
+            btnSave.Text = "Save";
+            btnDelete.Enabled = false;
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            if(sqlCon.State == ConnectionState.Closed)
+                sqlCon.Open();
+            SqlCommand sqlCmd = new SqlCommand("ContactCreateOrUpdate", sqlCon);
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+
+            sqlCmd.Parameters.AddWithValue("@ContactID",(hfContactID.Value==""?0:Convert.ToInt32(hfContactID.Value)));
+            sqlCmd.Parameters.AddWithValue("@Name",txtName.Text.Trim());
+            sqlCmd.Parameters.AddWithValue("@Mobile",txtMobile.Text.Trim());
+            sqlCmd.Parameters.AddWithValue("@Address",txtAddress.Text.Trim());
+            sqlCmd.ExecuteNonQuery();
+            sqlCon.Close();
+            Clear();
+            lblSuccessMessage.Text = hfContactID.Value == "" ? "Saved Successfully" : "Updated successfully";
+        }
+
+        void FillGridView()
+        {
+            if (sqlCon.State == ConnectionState.Closed)
+                sqlCon.Open();
+            SqlDataAdapter sqlDa = new SqlDataAdapter("ContactViewAll", sqlCon);
+            sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+            DataTable dtbl = new DataTable();
+            sqlDa.Fill(dtbl);
+            sqlCon.Close();
+            gvContact.DataSource = dtbl;
+            gvContact.DataBind();
+        }
+    }
+}
+
